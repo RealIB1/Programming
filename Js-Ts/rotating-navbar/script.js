94570a50a860a7a0b53d1b231c473bdaa331d969
1realib <ibeevpersie@gmail.com>
2022-08-06 09:11:08 +0000

    modified:   Js-Ts/rotating-navbar/index.html 	modified:   Js-Ts/rotating-navbar/script.js 	modified:   Js-Ts/rotating-navbar/style.css

--- Js-Ts/rotating-navbar/script.js
+++ Js-Ts/rotating-navbar/script.js
@@ -1,11 +1,12 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
+const body = document.getElementsByTagName("body");
 
-open.addEventListener('click', () => {
-    container.classList.add('show-nav')
-})
+open.addEventListener("click", () => {
+  container.classList.add("show-nav");
+});
 
-close.addEventListener('click', () => {
-    container.classList.remove('show-nav')
-})
+close.addEventListener("click", () => {
+  container.classList.remove("show-nav");
+});
